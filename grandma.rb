a2093db8b40f7603e83d23e82ac5aa56876b1514
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-05 16:11:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
- elsif phrase == "I LOVE YOU GRANDMA"
+ elsif phrase = "I LOVE YOU GRANDMA"
     "I LOVE YOU TOO PUMPKIN!"
   else phrase == "WHAT" || phrase == "WHAT DID YOU EAT TODAY?"
     "NO, NOT SINCE 1938!"
